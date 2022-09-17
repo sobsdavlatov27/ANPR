911b29ddc7ea9362223a17d2046562eb69bbc087
sobsdavlatov27 <107345762+sobsdavlatov27@users.noreply.github.com>
2022-09-17 15:29:24 +0500

    new function
    added function to detect objects from video stream

--- detect_func.py
+++ detect_func.py
@@ -1,10 +1,11 @@
 
-import cv2 
-import easyocr
-import numpy as np 
-import matplotlib.pyplot as plt 
 import os
 from datetime import datetime
+
+import cv2
+import easyocr
+import matplotlib.pyplot as plt
+import numpy as np
 from PIL import Image
 
 #load YOLOv3 and YOLOv3 custom
@@ -91,9 +92,6 @@ def readPlate(file):
         top_left = tuple([int(val) for val in detection[0][0]])
         bottom_right = tuple([int(val) for val  in detection[0][2]])
         text = detection[1]
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        img = cv2.rectangle(img, top_left, bottom_right, (0,255,0), 5)
-        img = cv2.putText(img, text, top_left, font, 2, (255,255,255), 2, cv2.LINE_AA)
         #saving the result into csv file
         with open('/Users/nighttwinkle/Documents/ANPR-V0.2/Saved detection/Car plate and color/numbers.csv', 'w') as n:
             n.write(text)
@@ -138,6 +136,62 @@ def detectCar(file):
             crop_img = img[y:y+h, x:x+w]
             saved = savefImg(crop_img)
             return saved
+#function to detect from video
+def detectFromVideo(file):
+
+    cap = cv2.VideoCapture(file)
+
+    while True: 
+        _, img = cap.read()
+        height, width, _ = img.shape
+
+        blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0,0,0), swapRB=True, crop=False)
+        net_car.setInput(blob)
+        output_layers_names = net.getUnconnectedOutLayersNames()
+        layerOutputs = net_car.forward(output_layers_names)
+
+        boxes = []
+        confidences = []
+        class_ids = []
+
+        for output in layerOutputs:
+            for detection in output:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.8:
+                    center_x = int(detection[0]*width)
+                    center_y = int(detection[1]*height)
+                    w = int(detection[2]*width)
+                    h = int(detection[3]*height)
+
+                    x = int(center_x - w/2)
+                    y = int(center_y - h/2)
+
+                    boxes.append([x, y, w, h])
+                    confidences.append((float(confidence)))
+                    class_ids.append(class_id)
+
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.2, 0.4)
+
+        if len(indexes)>0:
+            for i in indexes.flatten():
+                x, y, w, h = boxes[i]
+                label = str(vehicle_class[class_ids[i]])
+                confidence = str(round(confidences[i],2))
+                color = colors[i]
+                cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+                crop_img = img[y:y+h, x:x+w]
+                saved = savefImg(crop_img)
+                plate_img = detectPlate(saved)
+                plate_text = readPlate(plate_img)
+        cv2.imshow('Image', img)
+        if cv2.waitKey(1) & 0xFF==ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+   
 
 
 
@@ -145,7 +199,8 @@ def detectCar(file):
 
 
 
-    
+
+        
 
 
 
